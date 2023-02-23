9611883b96737b069791dbbb028bd24c041bf654
Елена Кливер <volandmart@mail.ru>
2023-02-23 16:10:52 +0500

    Revert "newTest"
    This reverts commit 65522b0589bf4528d4109b1a6be3212f97f2cc94.

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,38 +1,16 @@
 package ru.netology.service;
 
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
     @Test
 
-    public void CashBackFromRemain() {
+    public void CashBackFromRemain () {
         CashbackHackService service = new CashbackHackService();
-
         int expected = 200;
         int actual = service.remain(800);
 
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalcCashBack() {
-        CashbackHackService service = new CashbackHackService();
-
-        int expected = 0;
-        int actual = service.remain(1000);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalcCashBackIfOver() {
-        CashbackHackService service = new CashbackHackService();
-
-        int expected = 300;
-        int actual = service.remain(1700);
-
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(actual, expected);
     }
 }
