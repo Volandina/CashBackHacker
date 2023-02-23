65522b0589bf4528d4109b1a6be3212f97f2cc94
Елена Кливер <volandmart@mail.ru>
2023-02-23 15:57:29 +0500

    newTest

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,16 +1,38 @@
 package ru.netology.service;
 
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     @Test
 
-    public void CashBackFromRemain () {
+    public void CashBackFromRemain() {
         CashbackHackService service = new CashbackHackService();
+
         int expected = 200;
         int actual = service.remain(800);
 
-        org.junit.Assert.assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalcCashBack() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalcCashBackIfOver() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 300;
+        int actual = service.remain(1700);
+
+        assertEquals(expected, actual);
     }
 }
